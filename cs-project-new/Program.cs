8663e11bf80c27582303114d169ab4c25b88ee9c
Olivier Korach <olivier.korach@gmail.com>
2021-02-05 13:57:55 +0100

    Add code

--- cs-project-new/Program.cs
+++ cs-project-new/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cs_project_new
+namespace MyProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            int j = 1;
+            int k;
+            if (i == 0)
+            {
+                i = 1;
+            }
+            j += 1;
+            int[] n1 = new int[] { 2, 4 };
+            setValue(n1);
+            setValue(null);
+        }
+        static private void setValue(params int[] vals)
+        {
+            int value;
+            if (vals == null)
+            {
+                value = 0;
+            }
+            else
+            {
+                switch (vals.Length)
+                {
+                    case 1: value = vals[1]; break;
+                    case 2: value = vals[2]; break;
+                    default: value = 0; break;
+                }
+            }
+
         }
     }
+
 }
